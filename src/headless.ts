5215d1bde51bdd342a6f5f56a3a98b363f7e99ce
xonecas <seancaetanomartin@gmail.com>
2026-04-22 00:32:47 +0100

    feat: headless loop and shell tool

--- src/headless.ts
+++ src/headless.ts
@@ -1,9 +1,28 @@
-import { Context, stream } from "@mariozechner/pi-ai";
+import { complete, Context, stream, Tool, Type } from "@mariozechner/pi-ai";
 import { getApiKey } from "./oauth";
 import type { CliOptions } from "./types";
+import { $ } from "bun";
 
-export async function streamHeadless(options: CliOptions) {
+export async function streamHeadless(
+  options: CliOptions,
+  leave: (s: string) => void,
+) {
   const apiKey = await getApiKey(options);
+
+  const tools: Tool[] = [
+    {
+      name: "shell",
+      description:
+        "This is your command line, and uses the installed shell in your user's environment.",
+      parameters: Type.Object({
+        command: Type.String({
+          description:
+            "The command you want to run, ex: `ls`, `fd`, `find`, `grep`, `rg`, `cat`, `sed`, `bun`, etc",
+        }),
+      }),
+    },
+  ];
+
   const context: Context = {
     // TODO: Centralize prompts
     systemPrompt:
@@ -11,31 +30,69 @@ export async function streamHeadless(options: CliOptions) {
     messages: [
       { content: options.prompt ?? "", role: "user", timestamp: Date.now() },
     ],
-    tools: [],
+    tools,
   };
 
-  const s = stream(options.model, context, { apiKey });
-  for await (const ev of s) {
-    switch (ev.type) {
-      case "start":
-        console.log("Starting headless mode");
-        break;
-      case "text_start":
-        console.log("Answering...")
-        break;
-      case "thinking_start":
-        console.log("Thinking... or trying to...")
-        break;
-      case "done":
-        console.log("Done.")
-        break;
-      case "error":
-        console.log(`Error ${ev.error}`)
-        break;
+  while (true) {
+    const s = stream(options.model, context, {
+      apiKey,
+      reasoning: options.effort,
+    });
+    for await (const ev of s) {
+      switch (ev.type) {
+        case "text_start":
+          console.log("mini-coder headless");
+          console.log("-------------------");
+          console.log("Answering...");
+          break;
+        case "thinking_start":
+          console.log("Thinking...");
+          break;
+        case "toolcall_start":
+          console.log(`Calling tool...`);
+          break;
+        case "toolcall_end":
+          console.log(`${ev.toolCall.name}:\n${ev.toolCall.arguments}`);
+          break;
+        case "error":
+          console.log(`Error ${ev.error}`);
+          break;
+      }
+    }
+
+    const finalMessage = await s.result();
+    context.messages.push(finalMessage);
+    console.log(
+      finalMessage.content.map((m) => m.type === "text" && m.text).join("\n"),
+    );
+
+    const toolCalls = finalMessage.content.filter((b) => b.type === "toolCall");
+    for (const call of toolCalls) {
+      const result = (await $`${call.arguments}`).text();
+      context.messages.push({
+        role: "toolResult",
+        toolCallId: call.id,
+        toolName: call.name,
+        content: [{ type: "text", text: result }],
+        isError: false,
+        timestamp: Date.now(),
+      });
+    }
+
+    if (toolCalls.length > 0) {
+      const cont = await complete(options.model, context);
+      context.messages.push(cont);
+    }
+
+    console.log(
+      `Total tokens: ${finalMessage.usage.input} in, ${finalMessage.usage.output} out`,
+    );
+    console.log(`Cost: $${finalMessage.usage.cost.total.toFixed(4)}`);
+
+    if (["stop", "error", "aborted"].includes(finalMessage.stopReason)) {
+      console.log(`Reason for stopping: "${finalMessage.stopReason}"`)
+      const dur = Date.now() - context.messages[0].timestamp
+      leave(`Done. Took ${dur / 1000}s`);
     }
   }
-
-  const finalMessage = await s.result();
-  context.messages.push(finalMessage)
-  console.log(finalMessage.content)
 }
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,22 @@ import { handleArgv } from "./args.ts";
 import { streamHeadless } from "./headless.ts";
 import { initTUI } from "./tui.ts";
 
+function leave(msg: string) {
+  console.log(msg);
+  process.exit(0);
+}
+
 export async function main(): Promise<void> {
   const options = await handleArgv(process.argv.slice(2));
 
-  console.log(options);
-
   if (options.prompt) {
-    // TODO: Headless mode/non-interactiv
-    await streamHeadless(options);
+    await streamHeadless(options, leave);
     process.exit(0);
   }
 
   initTUI({
     onStop: () => {
-      console.log("Done.");
-      process.exit(0);
+      leave("Done.");
     },
   });
 }
