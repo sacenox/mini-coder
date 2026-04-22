742f1db9123131cd356b28b68fb1339d366d072d
xonecas <seancaetanomartin@gmail.com>
2026-04-22 08:45:52 +0100

    fix: Use Bun.spawn instead of bun shell

--- src/headless.ts
+++ src/headless.ts
@@ -1,7 +1,6 @@
 import { complete, Context, stream, Tool, Type } from "@mariozechner/pi-ai";
 import { getApiKey } from "./oauth";
 import type { CliOptions } from "./types";
-import { $ } from "bun";
 
 export async function streamHeadless(
   options: CliOptions,
@@ -11,9 +10,9 @@ export async function streamHeadless(
 
   const tools: Tool[] = [
     {
-      name: "shell",
+      name: "bash",
       description:
-        "This is your command line, and uses the installed shell in your user's environment.",
+        "This is your command line, runs commands in your user's environment.",
       parameters: Type.Object({
         command: Type.String({
           description:
@@ -33,16 +32,18 @@ export async function streamHeadless(
     tools,
   };
 
+  console.log("mini-coder headless");
+  console.log("-------------------");
+
   while (true) {
     const s = stream(options.model, context, {
       apiKey,
       reasoning: options.effort,
     });
+
     for await (const ev of s) {
       switch (ev.type) {
         case "text_start":
-          console.log("mini-coder headless");
-          console.log("-------------------");
           console.log("Answering...");
           break;
         case "thinking_start":
@@ -52,7 +53,9 @@ export async function streamHeadless(
           console.log(`Calling tool...`);
           break;
         case "toolcall_end":
-          console.log(`${ev.toolCall.name}:\n${ev.toolCall.arguments}`);
+          console.log(
+            `${ev.toolCall.name}:\n\t${ev.toolCall.arguments.command}`,
+          );
           break;
         case "error":
           console.log(`Error ${ev.error}`);
@@ -62,13 +65,18 @@ export async function streamHeadless(
 
     const finalMessage = await s.result();
     context.messages.push(finalMessage);
-    console.log(
-      finalMessage.content.map((m) => m.type === "text" && m.text).join("\n"),
-    );
+    const finalContent = finalMessage.content
+      .map((m) => m.type === "text" && m.text)
+      .filter((m) => Boolean(m));
+
+    if (finalContent.length > 0) console.log(finalContent.join("\n"));
 
     const toolCalls = finalMessage.content.filter((b) => b.type === "toolCall");
     for (const call of toolCalls) {
-      const result = (await $`${call.arguments}`).text();
+      const proc = Bun.spawn(["bash", "-c", call.arguments.command]);
+      await proc.exited;
+      const result = await proc.stdout.text();
+
       context.messages.push({
         role: "toolResult",
         toolCallId: call.id,
@@ -90,8 +98,8 @@ export async function streamHeadless(
     console.log(`Cost: $${finalMessage.usage.cost.total.toFixed(4)}`);
 
     if (["stop", "error", "aborted"].includes(finalMessage.stopReason)) {
-      console.log(`Reason for stopping: "${finalMessage.stopReason}"`)
-      const dur = Date.now() - context.messages[0].timestamp
+      console.log(`Reason for stopping: "${finalMessage.stopReason}"`);
+      const dur = Date.now() - context.messages[0].timestamp;
       leave(`Done. Took ${dur / 1000}s`);
     }
   }
